6fb7d3d8c251ab1819a7e5cee4324b74f1b47e40
toussaint10 <l.tona@alustudent.com>
2023-10-23 05:38:44 -0700

    Shell Programming Negpod 23

--- negpod_id-q1/backup-Negpod_ID.sh
+++ negpod_id-q1/backup-Negpod_ID.sh
@@ -1,29 +1,21 @@
-#!/bin/bash
-Backup to  2f05c1f8800b.3be8ebfc.alu-cod.online
-    
-# negpod_id-q1 
-backup_files="move-to-directory.sh"
-    
-#backup backup-Negpod_1.sh.
-dest="/summative/0923-2023S"
-    
-# Backupfiles.
-day= 23/10/2023
-hostname=2f05c1f8800b
-archive_file="2f05c1f8800b-23/10/2023.tgz"
-    
-# Print start status message.
-echo "Backing up backup-Negpod_1.sh to /summative/0923-2023S Backupfiles"
-date
-echo
-    
-# Backup the files using tar.
-tar czf /summative/0923-2023S /2f05c1f8800b-23/10/2023 backup-Negpod_1.sh.   
+#!/usr/bin/env bash
 
-# Print end status message.
-echo
-echo "Backup finished"
-date
-    
-# Long listing of files in $dest to check file sizes.
-ls -lh /summative/0923-2023S
+function load {
+}
+
+# Configuration for variables
+source_directory="negpod_0-q1"
+username="2f05c1f8800b"
+host="2f05c1f8800b.3be8ebfc.alu-cod.online"
+password="d4a1d225d0abda9549d8"
+dest="summative/0923-2023S"
+
+# Create the destination directory on the remote server if it doesn't exist
+sshpass -p "$password" ssh -o StrictHostKeyChecking=no "$username@$host" "mkdir -p /summative/0923-2023S"
+
+# Use rsync to backup the directory to the alu-cod.online server
+rsync -avz --delete -e "sshpass -p $password ssh -o StrictHostKeyChecking=no" "$source_directory" "$username@$host:$dest"
+
+echo -n "Backup completed. Ending Program ."
+load
+echo -e "\n\n **************************** Thank You For Using Our Program !!*****************************"
--- negpod_id-q1/move-to-directory.sh
+++ negpod_id-q1/move-to-directory.sh
@@ -1,2 +1,41 @@
-#!/bin/bash
-mv * shell_programming_neg23 negpod_id-q1
+#!/usr/bin/env bash
+
+# Function for program loader
+function load {
+}
+
+# Check if the directory exists
+folder="negpod_1-q1"
+
+# File initialization
+s_email='select-emails.sh'
+main='main.sh'
+file_path='students-list_0923.txt'
+file_save='student-emails.txt'
+
+if [ -f "$s_email" ] && [ -f "$main" ] && [ -f "$file_path" ] && [ -f "$file_save" ]; then
+    if [ -d "$folder" ]; then
+        echo -n "Moving Files "
+        load
+        mv -f "$main" "$s_email" "$file_save" "$file_path" "$folder"
+        echo -n "Backing up data to alu-cod.online "
+        load
+        ./backup-Negpod_1.sh
+    else
+        echo -n "Creating directory "
+        load
+        mkdir "$folder"
+
+        echo -n "Folder Created Successfully!! Moving Files "
+        load
+        mv -f "$main" "$s_email" "$file_save" "$file_path" 
+	"$folder"
+        echo -n "Backing up data to alu-cod.online "
+        load
+        ./backup-Negpod_1.sh
+    fi
+else
+    echo "Error: Your Files Don't Exist"
+    echo -n "Ending Programming "
+    load
+fi
